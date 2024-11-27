fc5df614e4448a972e2b50d5a524148e8784c109
Jojau12 <157808588+Jojau12@users.noreply.github.com>
2024-11-27 12:00:44 +0100

    Add files via upload

--- Sqlite.py
+++ Sqlite.py
@@ -1,25 +1,22 @@
 import sqlite3
 
-conn = sqlite3.connect('SQL/v2.db')
-cur = conn.cursor()
 
-cur.execute('SELECT * FROM PEOPLE')
-conn.commit()
-
-liste = cur.fetchall()
-
-cur.execute('SELECT * FROM NUMBERS')
-conn.commit()
-
-liste2 = cur.fetchall()
-
-cur.close()
-conn.close()
-
-print(liste, '\n', liste2)
 #Renvoi les personnes qui correspondent aux infos
 
+def people_correspondant(data):
 
+    conn = sqlite3.connect('SQL/v2.db')
+    cur = conn.cursor()
+    
+    cur.execute("SELECT * FROM PEOPLE WHERE ID_people LIKE %?% AND Nom LIKE ? AND Prenom LIKE ? AND Profession LIKE ? AND Date_de_naissance LIKE ?", data)
+    conn.commit()
+
+    liste2return=cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return liste2return
 #Renvoi les numéros de téléphones correspondant à une personne
 
 
@@ -39,3 +36,8 @@ print(liste, '\n', liste2)
 
 
 #Supprime une personne et ses numéros
+
+
+#########################################################################################################################
+
+print("Results:", people_correspondant(("zzz", "Michel", "Clément", "Chomeur", 2007)))
