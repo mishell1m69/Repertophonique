b26010aa6695dbe3befefccb32c7212127aae463
Clément MICHEL <159059762+mishell1m69@users.noreply.github.com>
2024-11-28 14:41:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,19 @@ def index():
 @app.route('/apropos')
 def apropos():
     return render_template("apropos.html")
+@app.route('/ajouter')
+def ajouter():
+    return render_template("ajouter.html")
+@app.route('/modifier')
+def modifier():
+    return render_template("modifier.html")
+@app.route('/supprimer')
+def supprimer():
+    return render_template("supprimer.html")
+@app.route('/howto')
+def howto():
+    return render_template("howto.html")
+@app.route('/leprojet')
+def leprojet():
+    return render_template("leprojet.html")
 app.run(debug=True)
