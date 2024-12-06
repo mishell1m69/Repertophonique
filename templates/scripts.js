14a29f930a4ff98b15597b52a9e43d7fc49de7be
mishell1m69 <clement.arno06@gmail.com>
2024-12-06 18:48:56 +0100

    Update

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,1 +1,13 @@
-
+function afficherPopup(event) {
+    event.preventDefault(); // empêche le lien de fonctionner normalement
+    var popup = confirm("Voulez-vous continuer ?");
+    if (popup) {
+      // si l'utilisateur appuie sur "OK", on continue normalement
+      window.location.href = event.target.href;
+    }
+  }
+  
+  // sélectionne le lien
+  var lienSpecifique = document.getElementById("liengithub");
+  
+  lienSpecifique.addEventListener("click", afficherPopup);
